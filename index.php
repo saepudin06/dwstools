35bea23493ec1cec2fd4252e5b8e1e169d5f7dd1
saepudin06 <saepudin06@gmail.com>
2019-11-12 16:05:37 +0700

    ''

--- index.php
+++ index.php
@@ -75,7 +75,7 @@ switch (ENVIRONMENT)
 {
 	case 'development':
 		// error_reporting(-1);
-		error_reporting(E_ALL ^ E_WARNING); 
+		error_reporting(E_ALL ^ (E_NOTICE | E_WARNING)); 
 		ini_set('display_errors', 1);
 	break;
 
