69af064a3369a4ff73c162c1d75c5a93a1f81fb9
saepudin06 <saepudin06@gmail.com>
2020-02-17 11:58:55 +0700

    ''

--- application/views/auth/login-2.php
+++ application/views/auth/login-2.php
@@ -47,41 +47,8 @@
                     </div>
                     <div class="flex-1" style="background: url(img/svg/pattern-1.svg) no-repeat center bottom fixed; background-size: cover;">
                         <div class="container py-4 py-lg-5 my-lg-5 px-4 px-sm-0">
-                            <div class="row">
-                                <div class="col col-md-6 col-lg-7 hidden-sm-down">
-                                    <!--<h2 class="fs-xxl fw-500 mt-4 text-white">
-                                        The simplest UI toolkit for developers &amp; programmers
-                                        <small class="h3 fw-300 mt-3 mb-5 text-white opacity-60">
-                                            Presenting you with the next level of innovative UX design and engineering. The most modular toolkit available with over 600+ layout permutations. Experience the simplicity of SmartAdmin, everywhere you go!
-                                        </small>
-                                    </h2>
-                                    <a href="#" class="fs-lg fw-500 text-white opacity-70">Learn more &gt;&gt;</a>
-                                    <div class="d-sm-flex flex-column align-items-center justify-content-center d-md-block">
-                                        <div class="px-0 py-1 mt-5 text-white fs-nano opacity-50">
-                                            Find us on social media
-                                        </div>
-                                        <div class="d-flex flex-row opacity-70">
-                                            <a href="#" class="mr-2 fs-xxl text-white">
-                                                <i class="fab fa-facebook-square"></i>
-                                            </a>
-                                            <a href="#" class="mr-2 fs-xxl text-white">
-                                                <i class="fab fa-twitter-square"></i>
-                                            </a>
-                                            <a href="#" class="mr-2 fs-xxl text-white">
-                                                <i class="fab fa-google-plus-square"></i>
-                                            </a>
-                                            <a href="#" class="mr-2 fs-xxl text-white">
-                                                <i class="fab fa-linkedin"></i>
-                                            </a>
-                                        </div>
-                                    </div>-->
-                                    
-                                    <img class="fs-xxl fw-500 mt-4 text-white" src="<?php echo base_url();?>assets/telkom.png" width="500px" height="300px">
-                                </div>
-                                <div class="col-sm-12 col-md-6 col-lg-5 col-xl-4 ml-auto">
-                                    <h1 class="text-white fw-300 mb-3 d-sm-block d-md-none">
-                                        Secure login
-                                    </h1>
+                            <div class="row align-items-center justify-content-center">
+                                <div class="col-sm-12 col-md-6 col-lg-5 col-xl-4">
                                     <div class="card p-4 rounded-plus bg-faded">
                                         <form id="js-login" novalidate="" action="<?php echo $login_url;?>" method="post">
 
@@ -108,12 +75,12 @@
                                                 <div class="invalid-feedback">Sorry, you missed this one.</div>
                                                 <div class="help-block">Your password</div>
                                             </div>
-                                            <div class="form-group text-left">
+                                            <!--<div class="form-group text-left">
                                                 <div class="custom-control custom-checkbox">
                                                     <input type="checkbox" class="custom-control-input" id="rememberme">
                                                     <label class="custom-control-label" for="rememberme"> Remember me for the next 30 days</label>
                                                 </div>
-                                            </div>
+                                            </div>-->
                                             <div class="row no-gutters">
                                                 <div class="col-lg-6 pr-lg-1 my-2">
                                                     <button id="js-login-btn" type="submit" class="btn btn-info btn-block btn-lg">Sign in</button>
